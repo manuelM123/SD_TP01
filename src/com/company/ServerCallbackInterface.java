4b55b95a27b03b92e7ed589b6c772589142244fe
Monty <46536704+monteiro141@users.noreply.github.com>
2022-04-21 12:54:27 +0100

    .

--- src/com/company/ServerCallbackInterface.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface ServerCallbackInterface extends java.rmi.Remote{
-    public void showOnServer(String s) throws java.rmi.RemoteException;
-    public void subscribe(String s, ClientCallbackInterface client) throws java.rmi.RemoteException;
-}
