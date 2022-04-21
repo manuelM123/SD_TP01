c8d86ea4a470277e657520df4cff852de720ca60
monteiro141 <46536704+monteiro141@users.noreply.github.com>
2022-04-21 12:20:05 +0100

    .

--- src/com/company/RMIServerNews.java
+++ src/com/company/RMIServerNews.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class RMIServerNews {
     public static void main(String[] args) {
@@ -14,14 +18,22 @@ public class RMIServerNews {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-
+        Properties prop;
         //instantiate remote object
+        prop = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/com/company/app.config")) {
+            prop.load(fis);
+        } catch (EOFException ex){
+            System.out.println("App.config file was read.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
         try{
             RMIInterfaceLogin login_remote = new RMIImplLogin();
             RMIInterfaceNews news_remote = new RMIImplNews();
             //register object in RMI registry
-            Naming.rebind("rmi://localhost/RMIImplLogin",login_remote);
-            Naming.rebind("rmi://localhost/RMIImplNews",news_remote);
+            Naming.rebind("rmi://"+prop.getProperty("app.mainServerIp")+":"+prop.getProperty("app.mainServerPort")+"/RMIImplLogin",login_remote);
+            Naming.rebind("rmi://"+prop.getProperty("app.mainServerIp")+":"+prop.getProperty("app.mainServerPort")+"/RMIImplNews",news_remote);
             System.out.println("Remote object ready");
         }catch (RemoteException | MalformedURLException e){
             System.out.println(e.getMessage());
